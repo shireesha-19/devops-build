2b9e6ca1e0712bddd1b53689298d9d1560649582
Shireesha <Shireeshagoud527@gmail.com>
2025-01-24 23:10:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u shireesha527 -p dimple@1234
-    docker tag appq shireesha527/dev
+    docker tag latest shireesha527/dev
     docker push shireesha527/dev
 
-elif [[ $GIT_BRANCH == "origin/master" ]]; then
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u shireesha527 -p dimple@1234
-    docker tag appq shireesha527/prod
-    docker push shireesha/prod
+    docker tag latest shireesha527/prod
+    docker push shireesha527/prod
 fi
